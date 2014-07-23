f49dd1ad23534b052d5939117fa372a5821e3f5e
Tyler <tw16g08@ecs.soton.ac.uk>
2014-07-23 17:33:49 +0200

    now able to determine host IP address and target file ready for request

--- mountainsensing/http-proxy-node/http-proxy.c
+++ mountainsensing/http-proxy-node/http-proxy.c
@@ -132,8 +132,8 @@ PROCESS_THREAD(webserver_nogui_process, ev, data)
 
 AUTOSTART_PROCESSES (&temp_process, &webserver_nogui_process);
 */
-static const char *TOP = "<html><head><title>ContikiRPL</title></head><body>\n";
-static const char *BOTTOM = "</body></html>\n";
+//static const char *TOP = "<html><head><title>ContikiRPL</title></head><body>\n";
+//static const char *BOTTOM = "</body></html>\n";
 #if BUF_USES_STACK
 static char *bufptr, *bufend;
 #define ADD(...) do {                                                   \
@@ -203,15 +203,11 @@ print_local_addresses(void)
 /*---------------------------------------------------------------------------
  * WGET CODE
  *--------------------------------------------------------------------------*/
+static char url[128];						//store url to be requested
+
+
 PROCESS_THREAD(wget_process, ev, data)
 {
-  static char url[128];						//store url to be requested
-  uip_ipaddr_t addr;						//store target ip address
-  unsigned char i;						//counter
-  static char host[32];						//store host name
-  char *file;							//store file name
-  register char *urlptr;					//pointer to point at characters
-
 
   PROCESS_BEGIN();
 
@@ -220,19 +216,54 @@ PROCESS_THREAD(wget_process, ev, data)
      PROCESS_YIELD();						//wait for a serial interupt
      if(ev == serial_line_event_message) {			//if we have recieved a line on serial port
         printf("received request: %s\n", (char *)data);		//print wget request
-	strcpy(url, (char *)data);				//coppy data to url
+	strcpy(url, "fe80::1/nodes");//(char *)data);				//coppy data to url
+
+       	fetch_url();						//fetch the data from the url
+
+     }
+#if RESOLV_FIXED
+     else if(ev == resolv_event_found) 
+     {
+	printf("resolv returned: ");
+     	/* Either found a hostname, or not. */
+     	if((char *)data != NULL &&
+        resolv_lookup((char *)data, NULL) == RESOLV_STATUS_CACHED) {
+		printf("successfull \n");
+        	fetch_url();
+	}
+     }
+#endif
+//     else if(ev == tcpip_event) {
+//     	webclient_appcall(data);
+//     }
+     else
+     {
+       printf(".");
+     } 
+  }
+
+  PROCESS_END();
+}
+/*-----------------------------------------------------------------------------------*/
+void fetch_url()
+{
+	uip_ipaddr_t addr;					//store target ip address
+	unsigned char i;					//counter
+	static char host[32];					//store host name
+	char *file;						//store file name
+	register char *urlptr;					//pointer to point at characters
 
 
-        /* Trim off any spaces in the end of the url. */
-        urlptr = url + strlen(url) - 1;				//work out length of url
-        while(*urlptr == ' ' && urlptr > url) {			//if url has whitespace
-          *urlptr = 0;						//remove whitespace
-          --urlptr;						//reduce length
-        }
-
-
-  	if(urlptr != url) {					//if there is a url
-
+	/* Trim off any spaces in the end of the url. */
+	urlptr = url + strlen(url) - 1;				//work out length of url
+	while(*urlptr == ' ' && urlptr > url) {			//if url has whitespace
+	  *urlptr = 0;						//remove whitespace
+	  --urlptr;						//reduce length
+	}
+	
+	
+	if(urlptr != url) {					//if there is a url
+	
 		/* See if the URL starts with http://, otherwise prepend it. */
 		if(strncmp(url, "http://", 7) != 0) {		//if string dosent start with http://
 			while(urlptr >= url) {			//for every character starting at the end
@@ -241,7 +272,7 @@ PROCESS_THREAD(wget_process, ev, data)
 			}
 			strncpy(url, "http://", 7);		//prepend http://
 		}
-
+	
 	
 		/* Find host part of the URL. */
 		urlptr = &url[7];				//start after http://
@@ -249,23 +280,23 @@ PROCESS_THREAD(wget_process, ev, data)
 			if(*urlptr == 0 ||			//if is not EOS
 			*urlptr == '/' ||			//if is start of path
 			*urlptr == ' ' ||			//is it is a space
-			*urlptr == ':') {			//if is start of port
+			*urlptr == '/') {			//if is start of port
 				host[i] = 0;			//add EOS terminator
 				break;				//break out of for loop
 			}
 			host[i] = *urlptr;			//add to strinf
 			++urlptr;				//increase length
 		}
-
+	
 		
 		printf("host: %s\n", (char *)host);		//print host name
-
 	
-
+	
+	
 		/* XXX: Here we should find the port part of the URL, but this isn't
 		 * currently done because of laziness from the programmer's side
 		 * :-) */ 
-
+	
 		/* Find file part of the URL. */
 		while(*urlptr != '/' && *urlptr != 0) {		//if not start of file
 			++urlptr;				//increment pointer
@@ -280,64 +311,28 @@ PROCESS_THREAD(wget_process, ev, data)
 		printf("file: %s\n", (char *)file);		//print file name
 
 
-  	}
-
-     }
-  }
-
-#ifdef LALALALALA
-
-#if UIP_UDP
-  /* First check if the host is an IP address. */
-  if(uiplib_ipaddrconv(host, &addr) == 0) {    
-    uip_ipaddr_t *addrptr;
-    /* Try to lookup the hostname. If it fails, we initiate a hostname */
-       lookup and print out an informative message on the
-       statusbar. 
-    if(resolv_lookup(host, &addrptr) != RESOLV_STATUS_CACHED) {
-      resolv_query(host);
-      puts("Resolving host...");
-      return;
-    }
-    uip_ipaddr_copy(&addr, addrptr);
-  }
-#else /* UIP_UDP */
-  uiplib_ipaddrconv(host, &addr);
-#endif /* UIP_UDP */
-
-  /* The hostname we present in the hostname table, so we send out the */
-     initial GET request. 
-  if(webclient_get(host, 80, file) == 0) {
-    puts("Out of memory error");
-  } else {
-    puts("Connecting...");
-  }
-}
-
-
-  while(1) {
-
-    PROCESS_WAIT_EVENT();
-  
-    if(ev == tcpip_event) {
-      webclient_appcall(data);
-#if UIP_UDP
-    } else if(ev == resolv_event_found) {
-      /* Either found a hostname, or not. */
-      if((char *)data != NULL &&
-        resolv_lookup((char *)data, NULL) == RESOLV_STATUS_CACHED) {
-        start_get();
-      } else {
-        puts("Host not found");
-        app_quit();
-      }
-#endif /* UIP_UDP */ 
-    }
-  }
-
+		/* First check if the host is an IP address. */
+		if(uiplib_ipaddrconv(host, &addr) == 0) {       //if not ip address
+#if RESOLV_FIXED
+			uip_ipaddr_t *addrptr;			
+			/* Try to lookup the hostname. If it fails, we initiate a hostname
+			 * lookup and print out an informative message on the
+			 * statusbar. */
+			if(resolv_lookup(host, &addrptr) != RESOLV_STATUS_CACHED) {
+				resolv_query(host);
+				printf("Resolving host...");
+			}
+			uip_ipaddr_copy(&addr, addrptr);
+#else
+			printf("unable to resolve\n");
+			return;
 #endif
-
-  PROCESS_END();
+		}
+	
+		printf("host IP addr: ");			//print host ip address
+		uip_debug_ipaddr_print(&addr);			//print address
+	
+		//webclient_get(host, 80, file); 			//get result
+		return;
+	}
 }
-/*-----------------------------------------------------------------------------------*/
-
