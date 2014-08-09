89a079d9c126d102f44cefa2c133fb567889dbaa
Philip Basford <pjb@ecs.soton.ac.uk>
2014-08-09 12:44:20 +0100

    Started creating app to test protocol buffer behaviour

---
+++ mountainsensing/sensors-test/protobuf-test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "contiki.h"
+#include "dev/i2cmaster.h"  // Include IC driver
+#include "dev/ms1-io.h"
+
+ 
+PROCESS (temp_process, "Test protocol buffer rs485 process");
+AUTOSTART_PROCESSES (&temp_process);
+/*---------------------------------------------------------------------------*/
+static struct etimer et;
+
+PROCESS_THREAD (temp_process, ev, data)
+{
+  PROCESS_BEGIN ();
+  ms1_io_init()
+  ms1_sense_on()
+  uart1_init(0);
+  {
+    while (1)
+      {
+        etimer_set(&et, TMP102_READ_INTERVAL);          // Set the timer
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));  // wait for its expiration
+ 
+	uart1_writearray((unsigned char*)"look sending",12);
+	printf ("sent");
+	
+      }
+  }
+  ms1_sense_off()
+  PROCESS_END ();
+}
