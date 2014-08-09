af2703d19c94f3bb2ef020ef1c2c81728a635139
Tyler Ward <tw16g08@ecs.soton.ac.uk>
2014-08-09 02:32:56 +0200

    Added rpl-border-router-imporved to project build script

--- mountainsensing/build-deployment.sh
+++ mountainsensing/build-deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#cd rpl-border-router-improved/ && make TARGET=z1 border-router
-#cd rpl-border-router-improved/ && make TARGET=z1-feshie border-router
+(cd rpl-border-router-improved/ && make TARGET=z1 border-router)
+(cd rpl-border-router-improved/ && make TARGET=z1-feshie border-router)
 
 (cd z1-sampler/ && make TARGET=z1 z1-sampler)
 (cd z1-sampler/ && make TARGET=z1-feshie z1-sampler)
