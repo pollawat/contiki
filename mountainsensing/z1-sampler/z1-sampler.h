5d08a33208a31ca7bd5aad3261c3defea0cdbcad
Philip Basford <pjb@ecs.soton.ac.uk>
2014-10-01 15:52:22 +0100

    Moved some #defs into header file

--- mountainsensing/z1-sampler/z1-sampler.h
+++ mountainsensing/z1-sampler/z1-sampler.h
@@ -1,4 +1,10 @@
 #define AVR_TIMEOUT_SECONDS 10
+#define LIVE_CONNECTION_TIMEOUT 300
+#define CONNECTION_RETRIES 3
+#define MAX_POST_SIZE 30
+
+#define SAMPLE_CONFIG 1
+#define COMMS_CONFIG 2
 
 static void avr_timer_handler(void *p);
 
